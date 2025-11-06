a7e72429b5f4fb1f49410a94b5cb9e0da6a8070e
Hamza <ansarihamza919@gmail.com>
2025-11-06 17:31:16 +0530

    TempFix:Email gmail added

--- configs/mailer.js
+++ configs/mailer.js
@@ -6,17 +6,10 @@ const nodemailer = require('nodemailer');
 console.log('SMTP Password = ', process.env.SMTP_PASS)
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
-    port: 587,
-    secure: false,               // STARTTLS (not SMTPS)
-    requireTLS: true,
+    service: 'gmail', // simpler than specifying host/port manually
     auth: {
-        user: 'tech@tsbi.in',      // must be the actual sign-in mailbox
-        pass: process.env.SMTP_PASS, // move the secret to env
-    },
-    tls: {
-        minVersion: 'TLSv1.2',     // Office 365 requires modern TLS
-        servername: 'smtp.office365.com',
+        user: 'thesmallbigidea1@gmail.com',
+        pass: 'meiu tjlt edoa sezi', // must be an App Password, not your regular Gmail password
     },
 });
 
